cce38add774563c1fff772b1c8da6175bdde7049
qingchang.tang <qingchang96@sina.com>
2020-06-20 20:56:02 +0800

    ..

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package thulacgo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+	fmt.Println("===============this is test=================")
+}
