bb8dc8cdef3fab48149b925c6ddf7dd5f7f2b5ed
qingchang.tang <qingchang96@sina.com>
2020-06-21 22:16:41 +0800

    ..

--- scripts/models.sh
+++ scripts/models.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 echo '======retrieve models======'
-pwd
-ls -l -a -h
 
 if [ ! -d "models" ]; then
-    echo '======models does not exists, download it======'
-    wget "http://thulac.thunlp.org/source/Models_v1_v2(v1_2).zip"
-    unzip -o "Models_v1_v2(v1_2).zip"
-else
-    echo '======models already exists======'
+  echo '======models does not exists, download it======'
+  wget "http://thulac.thunlp.org/source/Models_v1_v2(v1_2).zip"
+  unzip -o "Models_v1_v2(v1_2).zip"
 fi
 
 ls -l -a -h
