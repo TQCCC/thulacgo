678aa5a46cf745eadeea4a7aa0819c326c6bbfb6
qingchang.tang <qingchang96@sina.com>
2020-06-21 14:32:50 +0800

    ..

---
+++ scripts/models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '======retrieve models======'
+pwd
+ls -l -a -h
+
+if [ ! -d "models" ]; then
+    echo '======models does not exists, download it======'
+    wget "http://thulac.thunlp.org/source/Models_v1_v2(v1_2).zip"
+    unzip -o "Models_v1_v2(v1_2).zip"
+else
+    echo '======models already exists======'
+fi
+
+ls -l -a -h
+ls -l -a -h models
