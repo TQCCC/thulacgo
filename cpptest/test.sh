c08e4f01ddbb1774811e1ae8528f2ef5ad5b5f0e
qingchang.tang <qingchang96@sina.com>
2020-06-26 19:52:22 +0800

    ..

--- cpptest/test.sh
+++ cpptest/test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 cd ../
-pwd
-rm build/ -rf
-cmake -G "Unix Makefiles" -B build .
-ls -a -l -h
-
-cd build || exit
-make
-./ThulacgoTest
+rm build/ -rf && cmake -G "Unix Makefiles" -B build . && cd build && make && ./ThulacgoTest
